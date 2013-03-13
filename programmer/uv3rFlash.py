023cd6111004b4c523eb167c1940c4538d31abc1
lelazary <lelazary@yahoo.com>
2013-03-12 22:36:23 -0700

    Started the uv3r python to flash the uv3r. Still needs more work.

--- programmer/uv3rFlash.py
+++ programmer/uv3rFlash.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
-# srecparser.py
+# uv3rFlash.py 
+# a python program to assist with flashing the uv3r
+# Copyright (C) 2013 Lior Elazary - lior hat elazary.com
 #
-# Copyright (C) 2011 Gabriel Tremblay - initnull hat gmail.com
+#
+# Derived from srecparser.py  Copyright (C) 2011 Gabriel Tremblay - initnull hat gmail.com
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -27,82 +30,106 @@ import srecutils
 from optparse import OptionParser
 
 def __generate_option_parser():
-    usage_str = "usage: %prog [options] filename"
+    usage_str = "usage: %prog [options]"
     parser = OptionParser(usage=usage_str)
-    parser.add_option("-r", action="store_true",
-                      dest="readable", help="Human-readable output [default: %default]", default=False)
-    parser.add_option("-w", action="store_true",
-                      dest="wraparound", help="Wrap around characters [default: %default]", default=True)
-    parser.add_option("-c", action="store_true",
-                      dest="validate_checksum", help="Disable checksum validation [default: %default]", default=False)
-    parser.add_option("-l", action="store_true",
-                      dest="print_lines_number", help="Print line number [default: %default]", default=False)
-    parser.add_option("-d", action="store_true",
-                      dest="data_only", help="Data only [default: %default]", default=False)
-    parser.add_option("-o", metavar="OFFSET", dest="offset", help="Add OFFSET to bytes [default: %default]", default=0)
+    parser.add_option("-e", action="store_true",
+                      dest="erase", help="Erase chip [default: %default]", default=False)
+    parser.add_option("-w", action="store", type="string",
+                      dest="write", help="Write s-rec FILENAME to Flash memory ")
+    parser.add_option("-c", action="store", type="string",
+                      dest="config", help="Program CONFIG bits in hex")
+    parser.add_option("-r", action="store", type="string",
+                      dest="read", nargs=2, help="Read the flash memory from ADDR_BEGIN to ADDR_END in 4byte hex example C000 FFFF")
+    parser.add_option("-a", action="store",
+                      dest="auto", help="Erase, and then flash memory [default: %default]", default=False)
+    parser.add_option("-v", action="store", type="string",
+                      dest="verify", help="Verify that the filename VERIFY is in memory")
 
     return parser
 
+def eraseChip():
+  print "Erasing chip"
+
+def configChip(bits):
+  print "Config chip with %s " % bits
+
+def read(address):
+  print "Read chip from %s to %s" % (address[0], address[1])
+
+def write(filename):
+  print "Write %s " % filename
+  # open input file
+  scn_file = open(filename)
+  
+  linecount = 0
+  for srec in scn_file:
+      # Strip some file content
+      srec = srec.strip("\n")
+      srec = srec.strip("\r")
+  
+      # Validate checksum and parse record
+      if not srecutils.validate_srec_checksum(srec):
+          print "Invalid checksum found!"
+      else:
+          # Extract data from the srec
+          record_type, data_len, addr, data, checksum = srecutils.parse_srec(srec)
+  
+          if record_type == 'S1':
+              # Make a copy of the original data record for checksum calculation
+              raw_data = data
+  
+              # Apply offset (default is 0)
+              data = srecutils.offset_data(data, 0, 0, 0)
+  
+              # Get checksum of the new offset srec
+              raw_offset_srec = ''.join([record_type, data_len, addr, raw_data])
+              int_checksum = srecutils.compute_srec_checksum(raw_offset_srec)
+              checksum = srecutils.int_to_padded_hex_byte(int_checksum)
+  
+              #data = ''.join([record_type, data_len, addr, data, checksum])
+              print 'Program Addr %s %s %s\n' % (addr, data_len, data)
+  
+              data = ''.join([str(linecount), ': ', data])
+  
+              # output to file
+              print ''.join([data, '\n']),
+  
+  
+          # All the other record types
+          else:
+              output_str = ' No Action '.join([srec, '\n'])
+  
+              output_str = ''.join([str(linecount), ': ', output_str])
+  
+              print output_str,
+  
+  
+      # increment our fancy linecounter
+      linecount += 1
+  
+  scn_file.close()
 
 if __name__ == "__main__":
     parser = __generate_option_parser()
     (options, args) = parser.parse_args(sys.argv)
 
-    if len(args) <= 1 or len(args) > 2:
-        parser.print_help()
-        sys.exit()
 
-    # open input file
-    scn_file = open(args[1])
+    if options.erase:
+      eraseChip()
+      #Send command to erase chip
+    elif options.config is not None:
+      configChip(options.config)
+    elif options.auto:
+      eraseChip()
+      configChip("01")
+      write(options.auto)
+      print "Auto program"
+    elif options.write:
+      write(options.write)
+    elif options.read:
+      read(options.read)
+    else:
+      parser.print_help()
+      sys.exit()
 
-    linecount = 0
-    for srec in scn_file:
-        # Strip some file content
-        srec = srec.strip("\n")
-        srec = srec.strip("\r")
-
-        # Validate checksum and parse record
-        if options.validate_checksum and not srecutils.validate_srec_checksum(srec):
-            print "Invalid checksum found!"
-        else:
-            # Extract data from the srec
-            record_type, data_len, addr, data, checksum = srecutils.parse_srec(srec)
-
-            if record_type == 'S1' or record_type == 'S2' or record_type == 'S3':
-                # Make a copy of the original data record for checksum calculation
-                raw_data = data
-
-                # Apply offset (default is 0)
-                data = srecutils.offset_data(data, int(options.offset), options.readable, options.wraparound)
-
-                # Get checksum of the new offset srec
-                raw_offset_srec = ''.join([record_type, data_len, addr, raw_data])
-                int_checksum = srecutils.compute_srec_checksum(raw_offset_srec)
-                checksum = srecutils.int_to_padded_hex_byte(int_checksum)
-
-                print 'Program Addr %s %s %s\n' % (addr, data_len, data)
-                if not options.data_only:
-                    data = ''.join([record_type, data_len, addr, data, checksum])
-
-                if options.print_lines_number:
-                    data = ''.join([str(linecount), ': ', data])
-
-                # output to file
-                print ''.join([data, '\n']),
-
-
-            # All the other record types
-            else:
-                output_str = ''.join([srec, '\n'])
-
-                if options.print_lines_number:
-                   output_str = ''.join([str(linecount), ': ', output_str])
-
-                print output_str,
-
-
-        # increment our fancy linecounter
-        linecount += 1
-
-    scn_file.close()
 
